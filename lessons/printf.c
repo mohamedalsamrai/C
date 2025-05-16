7b7df3d1951e312d1be1ddb4b984e9924cdefb6e
mohamedalsamrai <st.compe.mohammed.a.f.awad@ced.nahrainuniv.edu.iq>
2025-05-16 13:15:51 +0300

    Format printf statements for better readability

--- lessons/printf.c
+++ lessons/printf.c
@@ -7,10 +7,18 @@ int main()
     float sneklang_default_pi = 3.141592;
     char *sneklang_title = "Sneklang";
     printf("Default max threads: %d\nCustom perms: %c\nConstant pi value: %f\nSneklang title: %s\n",
-         sneklang_default_max_threads, 
-         sneklang_default_perms, 
-         sneklang_default_pi, 
-         sneklang_title);
+           sneklang_default_max_threads,
+           sneklang_default_perms,
+           sneklang_default_pi,
+           sneklang_title); // %d for int, %c for char, %f for float, %s for string
 
+
+    float fahr, celsius;
+    fahr = 0;
+    for (fahr; fahr <= 300; fahr += 20)
+    {
+        celsius = (5.0 * (fahr - 32.0) / 9.0);
+        printf("%3.0f %6.2f\n", fahr, celsius); // 3 digits for fahr, 6 digits for celsius .2 digits after the decimal point
+    }
     return 0;
 }
